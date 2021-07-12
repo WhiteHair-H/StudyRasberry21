a7f1bd2163cadca5a61151ce206834ae6eb37327
WhiteHair-H <pddana@naver.com>
2021-07-12 15:29:37 +0900

    최초커밋

---
+++ test01.py
@@ -0,0 +1,24 @@
+n = 1
+name = 'Ha'
+n = n + 2
+value = 1.2 * n
+
+print('{0} = 1.2 * {1}'.format(value, n))
+print(name)
+
+greeting = 'Hello World'
+print(greeting[0])
+
+numbers = [0 , 1, 2, 3]
+names = ['Kim' , 'Lee' , 'Park' , 'Choi']
+array = numbers + names
+print(array)
+
+array[3] = 7
+print(array)
+
+# Tuple
+person = ('Kim' , 24 , 'male')
+print(person)
+print(person[1])
+person[1] = 45
